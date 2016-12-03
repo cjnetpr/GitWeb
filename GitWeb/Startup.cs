3f18da63b5c5c1ab1add54e7e56c912adcd662e8
Hassan <Hassan_nonahal@yahoo.com>
2016-12-03 14:57:10 -0800

    git commit 'first'

--- GitWeb/Startup.cs
+++ GitWeb/Startup.cs
@@ -4,8 +4,14 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(GitWeb.Startup))]
 namespace GitWeb
 {
+       
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+
+        public void GitT(IAppBuilder app)
+        {
             ConfigureAuth(app);
         }
     }
