b9ce3b256ee2dd384b7acfe20a962a79a633d3a0
Hassan <Hassan_nonahal@yahoo.com>
2016-12-17 23:22:26 -0800

    backOne

--- GitWeb/Default.aspx.cs
+++ GitWeb/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,7 @@ namespace GitWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("two");
+            Response.Write("one");
         }
     }
 }
